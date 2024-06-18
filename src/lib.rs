e8143cbcf8ae3555409396f7022b6f173cf09b3a
ohuu <ohuu@localhost.localdomain>
2024-06-18 16:40:24 +0100

    Allow users to specify stdout and stderr redirect
    - Added `redirect` property to [[processes]].
    - Fixed and added missing tests for unix platform.
    - Created new Allow users to specify where to redirect stdout and stderr.
    - Updated .sqlx files.
    - Updated cardamon config example.
    - Added cardamon config for windows and unix platforms.
    - Implemented `--file` option.

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,11 @@ pub mod metrics;
 pub mod metrics_logger;
 
 use anyhow::{anyhow, Context};
-use config::{ExecutionPlan, ProcessToObserve, ScenarioToExecute};
+use config::{ExecutionPlan, ProcessToObserve, Redirect, ScenarioToExecute};
 use data_access::{scenario_iteration::ScenarioIteration, DataAccessService};
 use dataset::ObservationDataset;
-use std::time;
-use subprocess::{Exec, NullFile};
+use std::{fs::File, path::Path, time};
+use subprocess::{Exec, NullFile, Redirection};
 
 /// Runs the given command as a detached processes. This function does not block because the
 /// process is managed by the OS and running separately from this thread.
@@ -21,17 +21,34 @@ use subprocess::{Exec, NullFile};
 /// # Returns
 ///
 /// The PID returned by the operating system
-fn run_command_detached(command: &str) -> anyhow::Result<u32> {
+fn run_command_detached(command: &str, redirect: &Option<Redirect>) -> anyhow::Result<u32> {
+    let redirect = redirect.unwrap_or(Redirect::File);
+
+    // break command string into POSIX words
+    let words = shlex::split(command).expect("Command string is not POSIX compliant.");
+
     // split command string into command and args
-    match &command.split(' ').collect::<Vec<_>>()[..] {
+    match &words[..] {
         [command, args @ ..] => {
-            let mut exec = Exec::cmd(command);
-            for arg in args {
-                exec = exec.arg(arg);
-            }
+            let exec = Exec::cmd(command).args(args);
+            // for arg in args {
+            //     exec = exec.arg(arg);
+            // }
+            //
+
+            let exec = match redirect {
+                Redirect::Null => exec.stdout(NullFile).stderr(NullFile),
+                Redirect::Parent => exec,
+                Redirect::File => {
+                    let out_file = File::create(Path::new("./.stdout"))?;
+                    let err_file = File::create(Path::new("./.stderr"))?;
+
+                    exec.stdout(Redirection::File(out_file))
+                        .stderr(Redirection::File(err_file))
+                }
+            };
 
             exec.detached()
-                .stdout(NullFile)
                 .popen()
                 .context("Failed to spawn detached process")?
                 .pid()
@@ -57,9 +74,10 @@ fn run_process(proc: &config::ProcessToExecute) -> anyhow::Result<Vec<ProcessToO
             name: _,
             containers,
             command,
+            redirect,
         } => {
             // run the command
-            run_command_detached(command)?;
+            run_command_detached(command, redirect)?;
 
             // return the containers as vector of ProcessToObserve
             Ok(containers
@@ -68,9 +86,13 @@ fn run_process(proc: &config::ProcessToExecute) -> anyhow::Result<Vec<ProcessToO
                 .collect())
         }
 
-        config::ProcessToExecute::BareMetal { name: _, command } => {
+        config::ProcessToExecute::BareMetal {
+            name: _,
+            command,
+            redirect,
+        } => {
             // run the command
-            let pid = run_command_detached(command)?;
+            let pid = run_command_detached(command, redirect)?;
 
             // return the pid as a ProcessToObserve
             Ok(vec![ProcessToObserve::Pid(pid)])
@@ -200,7 +222,10 @@ pub async fn run<'a>(
 
 #[cfg(test)]
 mod tests {
-    use crate::{config::ProcessToExecute, metrics_logger, run_process, ProcessToObserve};
+    use crate::{
+        config::{ProcessToExecute, Redirect},
+        metrics_logger, run_process, ProcessToObserve,
+    };
     use std::time::Duration;
     use sysinfo::{Pid, System};
 
@@ -235,6 +260,53 @@ mod tests {
         let process = ProcessToExecute::BareMetal {
             name: "sleep".to_string(),
             command: "powershell sleep 20".to_string(),
+        };
+        let processes_to_observe = run_process(&process)?;
+        let stop_handle = metrics_logger::start_logging(&processes_to_observe)?;
+
+        tokio::time::sleep(Duration::from_secs(10)).await;
+
+        let metrics_log = stop_handle.stop().await?;
+
+        assert!(!metrics_log.has_errors());
+        assert!(!metrics_log.get_metrics().is_empty());
+
+        Ok(())
+    }
+
+    #[test]
+    #[cfg(target_family = "unix")]
+    fn can_run_a_bare_metal_process() -> anyhow::Result<()> {
+        let process = ProcessToExecute::BareMetal {
+            name: "sleep".to_string(),
+            command: "sleep 15".to_string(),
+            redirect: Some(Redirect::Null),
+        };
+        let processes_to_observe = run_process(&process)?;
+
+        assert_eq!(processes_to_observe.len(), 1);
+
+        match processes_to_observe.first().expect("process should exist") {
+            ProcessToObserve::Pid(pid) => {
+                let mut system = System::new();
+                system.refresh_all();
+                let proc = system.process(Pid::from_u32(*pid));
+                assert!(proc.is_some());
+            }
+
+            _ => panic!("expected to find a process id"),
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    #[cfg(target_family = "unix")]
+    async fn log_scenario_should_return_metrics_log_without_errors() -> anyhow::Result<()> {
+        let process = ProcessToExecute::BareMetal {
+            name: "sleep".to_string(),
+            command: "sleep 20".to_string(),
+            redirect: Some(Redirect::Null),
         };
         let processes_to_observe = run_process(&process)?;
         let stop_handle = metrics_logger::start_logging(&processes_to_observe)?;
