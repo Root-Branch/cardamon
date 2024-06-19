c67d518250ac23bc96ad2990000e92a16efebc62
seal <will@kimbell.uk>
2024-06-19 13:48:40 +0100

    Finished swagger, start of logic

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
 mod errors;
 pub mod metric_routes;
 pub mod ui_routes;
+pub mod ui_types;
--- src/server/ui_routes.rs
+++ src/server/ui_routes.rs
@@ -1,29 +1,22 @@
 use super::errors::ServerError;
+use crate::server::ui_types::*;
 use axum::{
     extract::{Path, Query, State},
     Json,
 };
-use chrono::NaiveDateTime;
-use serde::Deserialize;
 use sqlx::SqlitePool;
 use tracing::instrument;
-use utoipa::ToSchema;
-use utoipa::{IntoParams, OpenApi};
-
-#[derive(Debug, Deserialize, ToSchema, IntoParams)]
-pub struct RunParams {
-    #[serde(rename = "startDate")]
-    start_date: Option<NaiveDateTime>,
-    #[serde(rename = "endDate")]
-    end_date: Option<NaiveDateTime>,
-}
+use utoipa::OpenApi;
 
 #[utoipa::path(
     get,
     path = "/api/runs",
-    params(RunParams),
+    params(
+        ("startDate" = Option<String>, Query, description = "Start date (String of NaiveDateTime)"),
+        ("endDate" = Option<String>, Query, description = "End date (String of NaiveDateTime)"),
+    ),
     responses(
-        (status = 200, description = "List of runs"),
+        (status = 200, description = "Data for runs within date range.", body = RunsResponse),
         (status = 500, description = "Internal Server Error")
     )
 )]
@@ -31,18 +24,66 @@ pub struct RunParams {
 pub async fn get_runs(
     State(pool): State<SqlitePool>,
     Query(params): Query<RunParams>,
-) -> anyhow::Result<String, ServerError> {
-    todo!()
+) -> anyhow::Result<Json<RunsResponse>, ServerError> {
+    // Returns runs with *not* scenario_name
+    Ok(Json(RunsResponse {
+        data: vec![
+            Runs {
+                metrics: vec![
+                    Metric {
+                        metric_type: "CO2".to_string(),
+                        type_field: MetricType::TOTAL,
+                        value: 0.81,
+                    },
+                    Metric {
+                        metric_type: "POWER".to_string(),
+                        type_field: MetricType::TOTAL,
+                        value: 1.23,
+                    },
+                    Metric {
+                        metric_type: "CPU".to_string(),
+                        type_field: MetricType::AVERAGE,
+                        value: 2.34,
+                    },
+                ],
+                start_time: "2023-06-15T10:30:00.000Z".to_string(),
+                id: "run_123".to_string(),
+                end_time: "2023-06-15T11:00:00.000Z".to_string(),
+            },
+            Runs {
+                metrics: vec![
+                    Metric {
+                        metric_type: "CO2".to_string(),
+                        type_field: MetricType::TOTAL,
+                        value: 0.92,
+                    },
+                    Metric {
+                        metric_type: "POWER".to_string(),
+                        type_field: MetricType::TOTAL,
+                        value: 1.45,
+                    },
+                    Metric {
+                        metric_type: "CPU".to_string(),
+                        type_field: MetricType::AVERAGE,
+                        value: 2.67,
+                    },
+                ],
+                start_time: "2023-06-16T09:15:00.000Z".to_string(),
+                id: "run_456".to_string(),
+                end_time: "2023-06-16T09:45:00.000Z".to_string(),
+            },
+        ],
+    }))
 }
 
 #[utoipa::path(
     get,
     path = "/api/runs/{runId}",
     params(
-        ("run_id", description = "ID of the run")
+        ("runId",description = "ID of the run")
     ),
     responses(
-        (status = 200, description = "List of scenarios for the specified run"),
+        (status = 200, description = "List of scenarios for the specified run", body = RunWithScenarioResponse),
         (status = 500, description = "Internal Server Error")
     )
 )]
@@ -50,18 +91,68 @@ pub async fn get_runs(
 pub async fn get_scenarios_for_run(
     Path(run_id): Path<String>,
     State(pool): State<SqlitePool>,
-) -> anyhow::Result<String, ServerError> {
-    todo!()
+) -> anyhow::Result<Json<RunWithScenarioResponse>, ServerError> {
+    // Returns runs *with* scenario_name
+    Ok(Json(RunWithScenarioResponse {
+        data: vec![
+            RunWithScenario {
+                metrics: vec![
+                    Metric {
+                        metric_type: "CO2".to_string(),
+                        type_field: MetricType::TOTAL,
+                        value: 0.81,
+                    },
+                    Metric {
+                        metric_type: "POWER".to_string(),
+                        type_field: MetricType::TOTAL,
+                        value: 1.23,
+                    },
+                    Metric {
+                        metric_type: "CPU".to_string(),
+                        type_field: MetricType::AVERAGE,
+                        value: 2.34,
+                    },
+                ],
+                start_time: "2023-06-15T10:30:00.000Z".to_string(),
+                id: "run_123".to_string(),
+                end_time: "2023-06-15T11:00:00.000Z".to_string(),
+                scenario_name: Some("Name1".to_string()),
+            },
+            RunWithScenario {
+                metrics: vec![
+                    Metric {
+                        metric_type: "CO2".to_string(),
+                        type_field: MetricType::TOTAL,
+                        value: 0.92,
+                    },
+                    Metric {
+                        metric_type: "POWER".to_string(),
+                        type_field: MetricType::TOTAL,
+                        value: 1.45,
+                    },
+                    Metric {
+                        metric_type: "CPU".to_string(),
+                        type_field: MetricType::TOTAL,
+                        value: 2.67,
+                    },
+                ],
+                scenario_name: Some("Name2".to_string()),
+                start_time: "2023-06-16T09:15:00.000Z".to_string(),
+                id: "run_456".to_string(),
+                end_time: "2023-06-16T09:45:00.000Z".to_string(),
+            },
+        ],
+    }))
 }
 
 #[utoipa::path(
     get,
     path = "/api/scenarios/{scenarioId}",
     params(
-        ("scenario_id", description = "ID of the scenario")
+        ("scenarioId", description = "ID of the scenario")
     ),
     responses(
-        (status = 200, description = "List of iterations for the specified scenario"),
+        (status = 200, description = "List of iterations for the specified scenario", body = ScenarioResponse),
         (status = 500, description = "Internal Server Error")
     )
 )]
@@ -69,32 +160,22 @@ pub async fn get_scenarios_for_run(
 pub async fn get_iterations(
     Path(scenario_id): Path<String>,
     State(pool): State<SqlitePool>,
-) -> anyhow::Result<String, ServerError> {
+) -> anyhow::Result<Json<ScenarioResponse>, ServerError> {
     todo!()
-}
-
-#[derive(Debug, Deserialize, ToSchema)]
-enum MetricsType {
-    TOTAL,
-    AVERAGE,
-}
-
-#[derive(Debug, Deserialize, ToSchema)]
-pub struct GetMetricsParams {
-    #[serde(rename = "startDate")]
-    start_date: Option<NaiveDateTime>,
-    #[serde(rename = "endDate")]
-    end_date: Option<NaiveDateTime>,
-    run_id: Option<String>,
-    r#type: MetricsType,
 }
 
 #[utoipa::path(
     get,
     path = "/api/metrics",
-    request_body = String,
+    params(
+        ("startDate" = Option<String>, Query, description = "Start date (String of NaiveDateTime)"),
+        ("endDate" = Option<String>, Query, description = "End date (String of NaiveDateTime)"),
+        ("runId" = Option<String>, Query, description = "Run ID"),
+        ("scenarioId" = Option<String>, Query, description = "Scenario ID "),
+        ("type" = MetricType, Query, description = "Type of metric")
+    ),
     responses(
-        (status = 200, description = "Metrics for runs, a specific run, or scenario"),
+        (status = 200, description = "Metrics based on the specified type (total or average) for runs, a specific run, or a specific scenario.", body = MetricResponse),
         (status = 500, description = "Internal Server Error")
     )
 )]
@@ -102,16 +183,21 @@ pub struct GetMetricsParams {
 pub async fn get_metrics(
     State(pool): State<SqlitePool>,
     Json(payload): Json<String>,
-) -> anyhow::Result<String, ServerError> {
+) -> anyhow::Result<Json<MetricResponse>, ServerError> {
     todo!()
 }
 
 #[utoipa::path(
     get,
     path = "/api/cpu-metrics",
-    request_body = String,
+    params(
+        ("startDate" = Option<String>, Query, description = "Start date (String of NaiveDateTime)"),
+        ("endDate" = Option<String>, Query, description = "End date (String of NaiveDateTime)"),
+        ("runId" = Option<String>, Query, description = "Run ID"),
+        ("scenarioId" = Option<String>, Query, description = "Scenario ID")
+    ),
     responses(
-        (status = 200, description = "CPU metrics for runs, a specific run, or scenario"),
+        (status = 200, description = "CPU metrics for runs, a specific run, or scenario", body = CpuMetricsResponse),
         (status = 500, description = "Internal Server Error")
     )
 )]
@@ -119,7 +205,7 @@ pub async fn get_metrics(
 pub async fn get_cpu_metrics(
     State(pool): State<SqlitePool>,
     Json(payload): Json<String>,
-) -> anyhow::Result<String, ServerError> {
+) -> anyhow::Result<Json<CpuMetricsResponse>, ServerError> {
     todo!()
 }
 
@@ -132,13 +218,17 @@ pub async fn get_cpu_metrics(
         get_metrics,
         get_cpu_metrics
     ),
-    components(
-        schemas(RunParams, MetricsType, GetMetricsParams)
-    ),
-    tags(
-        (name = "Runs", description = "API endpoints for managing runs"),
-        (name = "Scenarios", description = "API endpoints for managing scenarios"),
-        (name = "Metrics", description = "API endpoints for retrieving metrics")
-    )
+    components(schemas(
+        RunParams,
+        RunsResponse,
+        ScenarioResponse,
+        MetricType,
+        Scenario,
+        GetMetricsParams,
+        GetCpuMetricsParams,
+        RunWithScenarioResponse,
+        CpuMetricsResponse,
+        MetricResponse
+    ))
 )]
 pub struct ApiDoc;
