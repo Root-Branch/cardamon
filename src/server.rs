a186b793101af693abc22bc5eb07c11b27d078bc
ohuu <oliver@paperplane.software>
2024-08-27 12:04:50 +0100

    trying to get ui assets into crates

--- src/server.rs
+++ src/server.rs
@@ -2,59 +2,59 @@ mod errors;
 mod routes;
 mod types;
 
+use anyhow::Context;
 use axum::response::{Html, IntoResponse, Response};
-use axum::{routing::get, Router, http::header};
+use axum::{http::header, routing::get, Router};
 use http::{StatusCode, Uri};
 use rust_embed::Embed;
 use sea_orm::DatabaseConnection;
 use tracing::info;
-use anyhow::Context;
 
 #[derive(Embed, Clone)]
-#[folder = "ui/dist/"]
+#[folder = "src/public"]
 struct Asset;
 
 pub struct StaticFile<T>(pub T);
 
 impl<T> IntoResponse for StaticFile<T>
 where
-  T: Into<String>,
+    T: Into<String>,
 {
-  fn into_response(self) -> Response {
-    let path = self.0.into();
+    fn into_response(self) -> Response {
+        let path = self.0.into();
 
-    match Asset::get(path.as_str()) {
-      Some(content) => {
-        let mime = mime_guess::from_path(path).first_or_octet_stream();
-        ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
-      }
-      None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
+        match Asset::get(path.as_str()) {
+            Some(content) => {
+                let mime = mime_guess::from_path(path).first_or_octet_stream();
+                ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
+            }
+            None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
+        }
     }
-  }
 }
 
 // We use static route matchers ("/" and "/index.html") to serve our home
 // page.
 async fn index_handler() -> impl IntoResponse {
-  static_handler("/index.html".parse::<Uri>().unwrap()).await
+    static_handler("/index.html".parse::<Uri>().unwrap()).await
 }
 
 // We use a wildcard matcher ("/dist/*file") to match against everything
 // within our defined assets directory. This is the directory on our Asset
 // struct below, where folder = "examples/public/".
 async fn static_handler(uri: Uri) -> impl IntoResponse {
-  let mut path = uri.path().trim_start_matches('/').to_string();
+    let mut path = uri.path().trim_start_matches('/').to_string();
 
-  if path.starts_with("dist/") {
-    path = path.replace("dist/", "");
-  }
+    if path.starts_with("dist/") {
+        path = path.replace("dist/", "");
+    }
 
-  StaticFile(path)
+    StaticFile(path)
 }
 
 // Finally, we use a fallback route for anything that didn't match.
 async fn not_found() -> Html<&'static str> {
-  Html("<h1>404</h1><p>Not Found</p>")
+    Html("<h1>404</h1><p>Not Found</p>")
 }
 
 // Keep seperated for integraion tests
@@ -76,11 +76,11 @@ async fn create_app(db: &DatabaseConnection) -> Router {
 
     // let serve_assets = ServeEmbed::<Assets>::new();
     // Router::new().nest_service("/", serve_assets)
-        // .layer(
-        //     CorsLayer::new()
-        //         .allow_methods([Method::GET, Method::POST])
-        //         .allow_origin(Any),
-        // )
+    // .layer(
+    //     CorsLayer::new()
+    //         .allow_methods([Method::GET, Method::POST])
+    //         .allow_origin(Any),
+    // )
 }
 
 pub async fn start(port: u32, db: &DatabaseConnection) -> anyhow::Result<()> {
